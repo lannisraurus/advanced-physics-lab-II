69878bb171f94973569114fb943ad0203aa505d2
LannisRaurus <lannisraurus@gmail.com>
2024-09-30 20:24:37 +0100

    airy disk pgms

---
+++ main/ronchi_airy_fitter.cpp
@@ -0,0 +1,31 @@
+// C++ Libs
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+#include <math.h>
+
+// ROOT- CERN Includes
+#include "TApplication.h"
+#include "TSystem.h"
+
+#include "TCanvas.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TStyle.h"
+#include "TPaveText.h"
+
+#include "TH1F.h"
+#include "TH1.h"
+#include "TF1.h"
+#include "TROOT.h"
+
+
+
+int main(){
+
+
+
+	return 0;
+}
